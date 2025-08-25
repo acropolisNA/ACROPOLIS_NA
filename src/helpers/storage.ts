be78a43e704a038276c2dec64364af563cc03a08
CarlosCo_Dev <condorpasa26@gmail.com>
2025-08-24 23:51:58 -0500

    tmr

--- src/helpers/storage.ts
+++ src/helpers/storage.ts
@@ -1,5 +1,6 @@
 export const setLocal = (key:string,value:any) => sessionStorage.setItem(key,JSON.stringify(value))
 
+//@ts-ignore
 export const getLocal = (key:string) => JSON.parse(sessionStorage.getItem(key))
 
 export const getParams = () => new URLSearchParams(window.location.search)
